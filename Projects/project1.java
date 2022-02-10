f4a3c4cfab76d4b8924c00c9db860e1ed2bca6c2
Eric Pan <epan@sandiego.edu>
2022-02-10 10:28:10 -0800

    stuff

--- Projects/project1.java
+++ Projects/project1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.lang.reflect.Array;
 public class project1 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
@@ -13,10 +13,13 @@ public class project1 {
         System.out.println(message);
 
         String[] key_split = key.split(" ");
-        System.out.println(key_split);
-        String[] message_split = message.split("");
-        System.out.println(message_split);
-
+        //System.out.println(key_split[2]);
+        //String[] message_split = message.split("");
+        //System.out.println(message_split[0]);
+        char ascii = (char)Array.get(key_split,0);
+        int asciivalue1 = ascii;
+        
+        System.out.println("Ascii Value:" + asciivalue1);
 
 
         scan.close();
