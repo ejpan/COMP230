9401456537db57d533760499866301e5378cc3fb
Eric Pan <epan@sandiego.edu>
2022-02-10 11:43:53 -0800

    synced for proj1

--- Projects/project1.java
+++ Projects/project1.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class project1 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
@@ -14,12 +15,28 @@ public class project1 {
 
         String[] key_split = key.split(" ");
         //System.out.println(key_split[2]);
-        //String[] message_split = message.split("");
-        //System.out.println(message_split[0]);
-        char ascii = (char)Array.get(key_split,0);
-        int asciivalue1 = ascii;
+        String[] message_split = message.split("");
+        //System.out.println(message_split);
+        int key_length = key_split.length;
+        int key_counter = 0;
+        char[] message_char = message.toCharArray();
+        for (int message_counter = 0; message_counter <= message_split.length; message_counter++){ //Why can't I do char letter??
+            //System.out.println(letter);
+            char ch = message_char[message_counter];
+            int ascii_ch = ch;
+            ascii_ch += key_counter;
+            char converted_ch = (char) ascii_ch;
+            message_char[message_counter] = converted_ch;
+            //System.out.print("Ascii value: " + message_char[message_counter]);
         
-        System.out.println("Ascii Value:" + asciivalue1);
+        }
+        
+        System.out.println(toString(message_char));    
+            
+        
+        
+        
+        //System.out.println("Ascii Value:" + asciivalue1);
 
 
         scan.close();
