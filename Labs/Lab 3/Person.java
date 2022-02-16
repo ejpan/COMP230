fab67129de3b1ea159ec136399f8bc8817545a65
Eric Pan <epan@sandiego.edu>
2022-02-16 14:49:07 -0800

    lab3

---
+++ Labs/Lab 3/Person.java
@@ -0,0 +1,50 @@
+public class Person 
+{
+    private String Name;
+    private int Age;
+    private double Income;
+
+    public Person() 
+    {
+        String Name = "";
+        int Age = 0;
+        double Income = 0;
+
+    }
+    public Person(String n, int a, double i)
+    {
+        String Name = n;
+        int Age = a;
+        double Income = i;
+    }
+    public String getName()
+    {
+        return Name;
+    }
+    public int getAge()
+    {
+        return Age;
+    }
+    public double getIncome()
+    {
+        return Income;
+    }
+    public void setName(String n)
+    {
+        Name = n;
+    }
+    public void setAge(int a)
+    {
+        Age = a;
+    } 
+    public void setIncome(double i)
+    {
+        Income = i;
+    }
+    public boolean equals(Object p)
+    {
+        return (Name = p.Name) && ()
+    }
+}   
+
+
