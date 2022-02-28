fd4e1a66e5f00dc18f659aca108c8d82d31fff70
Gabe Seidl <gseidl@sandiego.edu>
2022-02-28 12:47:38 -0800

    made my version of proj

--- Projects/proj2_gabe.java
+++ Projects/proj2_gabe.java
@@ -1,11 +1,15 @@
 import java.io.*;
 import java.util.Scanner;
-public class proj2_gabe {
-    public static void main(String[] args){
+public class proj2_gabe 
+{
+    public static void main(String[] args)
+    {
         Scanner input = new Scanner(System.in);
         boolean again = true;
-            while (again){
-                try{
+        while (again)
+        {
+            try
+            {
                 System.out.println("Please enter the name of an ASCII file that contains a sorted list of integer numbers, one per line: ");
                 String input_file = input.nextLine();
                 System.out.println("Please enter the name of the output file that saves the result from the program: ");
@@ -16,32 +20,42 @@ public class proj2_gabe {
                 PrintWriter pw = new PrintWriter(new FileOutputStream(out));
                 int total = 0;
                 int count = 0;
-                if (reader.hasNextLine()){
+                if (reader.hasNextLine())
+                {
                     int next_num = reader.nextInt();
                     int last_num = next_num - 1;
-                    while (reader.hasNextLine()){
-                        if (last_num != next_num){
+                    while (reader.hasNextLine())
+                    {
+                        if (last_num != next_num)
+                        {
                             pw.println(next_num);
                             last_num = next_num;
                             total += next_num;
                             count += 1;
-                        }else{
+                        }
+                        else
+                        {
                             next_num = reader.nextInt();
                         }
                     }
-                    if (last_num != next_num){
+                    if (last_num != next_num)
+                    {
                         pw.println(next_num);
                         last_num = next_num;
                         total += next_num;
                         count += 1;
                     }
-                    pw.println(total/count/1.0);
-                }else{
+                    pw.println(total/1.0/count);
+                }
+                else
+                {
                     pw.println(0);
                 }
                 reader.close();
                 pw.close();
-            }catch (IOException e){
+            }
+            catch (IOException e)
+            {
                 System.out.println(e.getMessage());
             }
             System.out.println("Do you want to run the program again? (y for yes, n for no): ");
@@ -50,9 +64,9 @@ public class proj2_gabe {
             {
                 again = false;
             }
-    }   
-    input.close();
-       
+        }   
+        input.close();
+    }      
 }
-}
+
 
