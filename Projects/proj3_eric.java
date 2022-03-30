4964772c5b1594a2b562f771db005be25d68a7ee
Gabe Seidl <gseidl@sandiego.edu>
2022-03-30 15:30:53 -0700

    added secondAlg

--- Projects/proj3_eric.java
+++ Projects/proj3_eric.java
@@ -24,6 +24,25 @@ public class proj3_eric {
         return maxSum;
     }
 
+    static int secondAlg (int[] nums)
+    {
+        int maxSum = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            int sum = 0;
+            for(int j = i; j < nums.length; j++)
+            {
+                sum += nums[j];
+                if (sum > maxSum)
+                {
+                    maxSum = sum;
+                }
+            }
+        }
+        return maxSum;
+    }
+
+
     public static void main(String[] args) 
     {
         try
