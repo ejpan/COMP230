cb261e6c5a004c2c02abbbd2cc78ee5a9dae4281
Eric Pan <epan@sandiego.edu>
2022-04-03 15:34:20 -0700

    proj3 left and right

--- Projects/proj3_eric.java
+++ Projects/proj3_eric.java
@@ -49,14 +49,75 @@ public class proj3_eric {
         return maxSum;
     }
 
-    static int thirdAlg (int[] nums)
+    static void thirdAlg (int[] nums)
     {
         int[] first = Arrays.copyOfRange(nums, 0, nums.length/2);
-        int[] second = Arrays.copyOfRange(nums, 0, nums.length/2);
-        int maxLeftSum = thirdAlg(first);
-        int maxRightSum = thirdAlg(second);
+        int[] second = Arrays.copyOfRange(nums, nums.length/2, nums.length);
+        int[] maxLeftSum = leftSum(first);
+        int[] maxRightSum = rightSum(second);
+        System.out.println(maxLeftSum[0]);
+        System.out.println(maxRightSum[1]);
+        // maxSum = 0
+        // sum = left + right  : return maxSum
+        // else (left) 
         
+    }
+
+    static int[] leftSum (int[] nums)
+    {
+        if (nums.length == 1){
+            int[] digit = {nums[0],nums[0]};
+            return digit;
+        }else{
+            int[] first = Arrays.copyOfRange(nums, 0, nums.length/2);
+            //System.out.println(Arrays.toString(first));
+            int[] second = Arrays.copyOfRange(nums, nums.length/2, nums.length);
+            //System.out.println(Arrays.toString(second));
+            int[] left = leftSum(first);
+            int[] right = leftSum(second);
+            int sum = left[1] + right[1];
+            if (left[1] + right[0] > left[0])
+            {
+                left[0] = left[1] + right[0];
+            }else if (sum > left[0])
+            {
+                left[0] = left[1] + right[1];
+            }
+            int[] sums = {left[0], sum};
+            //System.out.println(Arrays.toString(sums));
+            return sums;
+        }
         
+
+    }
+
+    static int[] rightSum (int[] nums)
+    {
+        if (nums.length == 1){
+            int[] digit = {nums[0],nums[0]};
+            return digit;
+        }else{
+            int[] first = Arrays.copyOfRange(nums, nums.length/2, nums.length);
+            int[] second = Arrays.copyOfRange(nums, 0, nums.length/2);
+            //System.out.println(Arrays.toString(first));
+            
+            //System.out.println(Arrays.toString(second));
+            int[] right = rightSum(first);
+            int[] left = rightSum(second);
+            int sum = left[0] + right[0];
+            if (right[0] + left[1] > right[1])
+            {
+                right[1] = right[0] + left[1];
+            }else if (sum > right[0])
+            {
+                right[1] = left[0] + right[0];
+            }
+            int[] sums = {sum, right[1]};
+            System.out.println(Arrays.toString(sums));
+            return sums;
+        }
+        
+
     }
 
     static int fourthAlg (int[] nums)
@@ -105,8 +166,8 @@ public class proj3_eric {
                 int temp = Integer.parseInt(numbers_line[i]);
                 numbers[i] = temp;
             }
-            System.out.println(thirdAlg(numbers));
-            System.out.println(fourthAlg(numbers));
+            thirdAlg(numbers);
+            //System.out.println(fourthAlg(numbers));
 
             input.close();
             reader.close();
